174b22670f4a5c8cb513a1c32c6af289d4ad32a9
Pedro22D0W <110983216+Pedro22D0W@users.noreply.github.com>
2022-08-26 09:58:18 -0300

    Update Calculadora .py

--- Calculadora .py
+++ Calculadora .py
@@ -1,3 +1,8 @@
 op = input('escolha a operação(+,-,*,/):')
 x = input('primeiro membro')
 y = input('segundo membro')
+
+soma = int(x) + int(y)
+subitração = int(x) - int(y)
+multiplicação = int(x) * int(y)
+divisão= int(x) / int(y)
